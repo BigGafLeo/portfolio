160cc6c11e78dcb755dda1c1fb95dc5529c9b25b
BigGafLeo <karoldzwon@gmail.com>
2025-02-17 19:37:04 +0100

    Vercel statistics

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import AppRouter from './router';
+import { Analytics } from '@vercel/analytics/react';
 import GlobalStyle from './styles/GlobalStyles';
 
 const queryClient = new QueryClient();
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
+        <Analytics />
         <GlobalStyle />
         <AppRouter />
       </ThemeProvider>
